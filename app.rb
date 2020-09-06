1a85fd84bf7d29d7687b72f2fe4103949d04004d
edmundo papi <edmundopierre@yahoo.com>
2020-09-06 09:18:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ class App < Sinatra::Base
     "My name is Pierre"
   end
 
-  get '/hometown'
+  get '/hometown'do
     "My hometown is Makokou"
   end
 
-get '/favorite-song'
+get '/favorite-song'do
    "My favorite-song is Bolo"
 end
 end
